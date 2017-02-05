a3605cd0883507b5f62ed4ef32c7080fc7c7bd73
Rudy Matela <rudy@matela.com.br>
2017-02-05 15:55:54 +0000

    haskell: improve makefile (import from logic)

---
+++ haskell/mk/depend.mk
@@ -0,0 +1,65 @@
+eg/raw/chaser.o: \
+  Tankode/Raw.hs \
+  Tankode/Constants.hs \
+  eg/raw/chaser.hs
+eg/raw/chaser: \
+  Tankode/Raw.hs \
+  Tankode/Constants.hs \
+  eg/raw/chaser.hs
+eg/raw/escaper.o: \
+  Tankode/Raw.hs \
+  Tankode/Constants.hs \
+  eg/raw/escaper.hs
+eg/raw/escaper: \
+  Tankode/Raw.hs \
+  Tankode/Constants.hs \
+  eg/raw/escaper.hs
+eg/raw/left-turner.o: \
+  Tankode/Raw.hs \
+  Tankode/Constants.hs \
+  eg/raw/left-turner.hs
+eg/raw/left-turner: \
+  Tankode/Raw.hs \
+  Tankode/Constants.hs \
+  eg/raw/left-turner.hs
+eg/raw/right-turner.o: \
+  Tankode/Raw.hs \
+  Tankode/Constants.hs \
+  eg/raw/right-turner.hs
+eg/raw/right-turner: \
+  Tankode/Raw.hs \
+  Tankode/Constants.hs \
+  eg/raw/right-turner.hs
+eg/raw/sitting-duck.o: \
+  Tankode/Raw.hs \
+  Tankode/Constants.hs \
+  eg/raw/sitting-duck.hs
+eg/raw/sitting-duck: \
+  Tankode/Raw.hs \
+  Tankode/Constants.hs \
+  eg/raw/sitting-duck.hs
+eg/raw/wal.o: \
+  Tankode/Raw.hs \
+  Tankode/Constants.hs \
+  eg/raw/wal.hs
+eg/raw/wal: \
+  Tankode/Raw.hs \
+  Tankode/Constants.hs \
+  eg/raw/wal.hs
+eg/raw/zigzagger.o: \
+  Tankode/Raw.hs \
+  Tankode/Constants.hs \
+  eg/raw/zigzagger.hs
+eg/raw/zigzagger: \
+  Tankode/Raw.hs \
+  Tankode/Constants.hs \
+  eg/raw/zigzagger.hs
+Tankode/Basic.o: \
+  Tankode/Raw.hs \
+  Tankode/Constants.hs \
+  Tankode/Basic.hs
+Tankode/Constants.o: \
+  Tankode/Constants.hs
+Tankode/Raw.o: \
+  Tankode/Raw.hs \
+  Tankode/Constants.hs
