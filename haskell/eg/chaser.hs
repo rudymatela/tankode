7a7967ecd77ee6045257b162ebdda815a7f841e2
Rudy Matela <rudy@matela.com.br>
2017-02-05 17:38:24 +0000

    improve

--- haskell/eg/chaser.hs
+++ haskell/eg/chaser.hs
@@ -12,18 +12,16 @@ ident = Id
   }
 
 chaser :: Tankode () -- :: Input () -> Output ()
-chaser Input {enemy = Just d} = output
-  { shoot = 1
-  , accel = case () of _ | d > 1     ->  1
-                         | d < 1     -> -1
-                         | otherwise ->  0
-  }
-chaser Input {enemy = Nothing, speed = s} = output
-  { body  = 1
-  , accel = case () of _ | s > 0     -> -1
-                         | s < 0     ->  1
-                         | otherwise ->  0
-  }
+chaser Input {enemy = Just d} = output {shoot = 1, accel = a}
+  where
+  a | d > 1     =  1
+    | d < 1     = -1
+    | otherwise =  0
+chaser Input {enemy = Nothing, speed = s} = output {body  = 1, accel = a}
+  where
+  a | s > 0     = -1
+    | s < 0     =  1
+    | otherwise =  0
 
 main :: IO ()
 main = run ident chaser ()
