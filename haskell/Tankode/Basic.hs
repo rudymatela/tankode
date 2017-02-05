74e6c9b8a8a9d6a8d2606aac0539682827d08b50
Rudy Matela <rudy@matela.com.br>
2017-02-05 14:54:52 +0000

    update basic stub

--- haskell/Tankode/Basic.hs
+++ haskell/Tankode/Basic.hs
@@ -11,3 +11,34 @@ import System.IO
 import qualified Tankode.Raw as R
 import Tankode.Raw (Id(..), Colour, IncDec(..))
 import Tankode.Constants
+
+type Tankode s   = Input -> s ->    (s,Output)
+type TankodeIO s = Input -> s -> IO (s,Output)
+
+data Input  = Input
+  { life  :: Rational
+  , speed :: Rational
+  , enemy :: Maybe Rational
+  , wall  :: Maybe Rational
+  , gunHeading   :: Rational
+  , radarHeading :: Rational
+  }
+  deriving Show
+
+data Output = Output
+  { accel :: IncDec
+  , body  :: IncDec
+  , gun   :: Rational
+  , radar :: Rational
+  , shoot :: Rational
+  }
+  deriving Show
+
+output :: Output
+output = Output
+  { accel = Nul
+  , body  = Nul
+  , gun   = 0
+  , radar = 0
+  , shoot = 0
+  }
