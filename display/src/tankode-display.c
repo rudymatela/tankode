2abaea3197c13dc9f14bb2f6aca7153388c0331e
Rudy Matela <rudy@matela.com.br>
2017-02-12 15:42:02 +0000

    fix display

--- display/src/tankode-display.c
+++ display/src/tankode-display.c
@@ -99,8 +99,8 @@ void update_state()
 void render_and_reschedule(int val)
 {
 	glutTimerFunc(1000 / FPS, render_and_reschedule, val);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); update_state(); draw(); glAccum(GL_LOAD,  1./6.);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); update_state(); draw(); glAccum(GL_ACCUM, 1./6.);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); update_state(); draw(); glAccum(GL_LOAD,  1./2.);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); update_state(); draw(); glAccum(GL_ACCUM, 1./2.);
 	glAccum(GL_RETURN, 1.);
 	glutSwapBuffers();
 }
