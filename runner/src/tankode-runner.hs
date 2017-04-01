f7ba4a2163ada8eda6ed452b8fb43cb72b5f4888
Rudy Matela <rudy@matela.com.br>
2017-04-01 20:06:29 +0100

    Add copyright notice on main programs.

--- runner/src/tankode-runner.hs
+++ runner/src/tankode-runner.hs
@@ -1,6 +1,21 @@
--- tankode's logic, in Haskell
--- started by Rudy Matela on 2016-12-22 18:30
-
+-- tankode-runner.hs -- main runner of the Tankode game
+--
+-- Copyright (C) 2016, 2017  Rudy Matela
+--
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU Lesser General Public License
+-- version 2.1, as published by the Free Software Foundation.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Lesser General Public License for more details.
+--
+-- You should have received a copy of the GNU Lesser General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+--
+-- (development started by Rudy Matela on 2016-12-22 18:30)
 import Tankode
 import Tankode.Data
 import Tankode.Show
