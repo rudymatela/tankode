4ea913aae797e1f4b609450826bef7278b4dd22c
Rudy Matela <rudy@matela.com.br>
2017-04-01 20:14:26 +0100

    runner/display: add copyright notice

--- runner/display/src/audio.h
+++ runner/display/src/audio.h
@@ -1,4 +1,19 @@
-/* simple wrapper to openal */
+/* audio.h -- a simple wrapper for OpenAL
+ *
+ * Copyright (C) 2017  Rudy Matela
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef _AUDIO_H
 #define _AUDIO_H
 
--- runner/display/src/tankode.h
+++ runner/display/src/tankode.h
@@ -1,3 +1,22 @@
+/* tankode-display.c -- objects of the displayer of the Tankode game
+ *
+ * Copyright (C) 2016, 2017  Rudy Matela
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * (development started by Rudy Matela on 2016-12-22 18:30)
+ */
 #ifndef _TANKODE_H
 #define _TANKODE_H
 
