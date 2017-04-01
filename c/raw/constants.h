27ed1d110cb29e859d1a5d3fde2f16ed2216ea60
Rudy Matela <rudy@matela.com.br>
2017-04-01 21:20:07 +0100

    c: improve docs of the C interface

--- c/raw/constants.h
+++ c/raw/constants.h
@@ -1,6 +1,18 @@
-/*
- * Not all constants are ported at the moment.
- * see `haskell/Tankode/Constants.hs` for a full list
+/* constants.h -- constants of the Tankode game
+ *
+ * Copyright (C) 2017  Rudy Matela
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef _CONSTANTS_H
 #define _CONSTANTS_H
